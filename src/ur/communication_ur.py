6e21ac38c34754a9070068563ed3637206e8d273
Bisssen <bisssen@gmailcom>
2020-10-07 10:49:47 +0200

    Merged python 2 and 3 version

--- src/ur/communication_ur.py
+++ src/ur/communication_ur.py
@@ -1,29 +1,34 @@
 import socket
 import struct
 import threading
+import sys
 
-from . import config_robot as cfg
+from . import config_ur
+
 
 class communication_thread():
     def __init__(self):
-        # Whether the program is run in python 2 or not 
+        # Whether the program is run in python 2 or not
         self.python_2 = (sys.version_info.major == 2)
 
         # Creating the socket
         self.socket_robot = socket.socket(socket.AF_INET,
                                           socket.SOCK_STREAM)
-        self.socket_robot.connect((cfg.SOCKETS['host ip'],
-                                   cfg.SOCKETS['port send']))
+        self.socket_robot.connect((config_ur.IP,
+                                   config_ur.PORT))
 
+        # The thread keeps going as long as this variable is true
         self.running = True
 
+        # The data which the thread optains from the ur
         self.data = 0
 
+        # The thread which keeps receiving data
         self.receive_thread = threading.Thread(target=self.receive)
 
+        # Starting the Thread
         print('    Starting communication thread...')
         self.receive_thread.start()
-
 
     def receive(self):
         while self.running:
@@ -36,32 +41,29 @@ class communication_thread():
         self.socket_robot.close()
 
 
-######################### READING DATA FROM ROBOT ###############################
 def transform_data_point(data, data_type):
-    # message size is the first 4 bits being sent
-    if data_type is 'message_size':
-        data = data[:4]
-        data = data.hex()
-        if len(data) == 8:
-            data = struct.unpack('!i', bytes.fromhex(data))[0]
-            return data
-        else:
-            return 0
+    num = config_ur.DATA_MAP[data_type]
+    data = data[num:num+8]
+
+    # Convert the data from \x hex notation to plain hex
+    if self.python_2:
+        data = data.encode("hex")
     else:
-        num = cfg.DATA_MAP[data_type]
-        data = data[num:num+8]
-        data = data.hex() #convert the data from \x hex notation to plain hex
-        if len(data) == 16:
-            data = struct.unpack('!d', bytes.fromhex(data))[0]
-            return data
+        data = data.hex()
+
+    if len(data) == 16:
+        if self.python_2:
+            data = struct.unpack('!d', data.decode("hex"))[0]
         else:
-            return 0
+            data = struct.unpack('!d', bytes.fromhex(data))[0]
+        return data
+    else:
+        return 0
 
 
 def transform_data(data):
     data_string = ''
-    for data_type in cfg.DATA_MAP:
+    for data_type in config_ur.DATA_MAP:
         data_point = transform_data_point(data, data_type)
         data_string += data_type + ':' + str(data_point) + ';'
     return data_string
-
